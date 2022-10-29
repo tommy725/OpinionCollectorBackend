df6f5ed1c0a2e3e7304a48d6d4e3ea9c60d3c312
Tommy <afteryou725@gmail.com>
2022-10-29 20:37:23 +0200

    Add swagger

--- src/main/java/pl/opinion_collector/backend/OpinionCollectorBackendApplication.java
+++ src/main/java/pl/opinion_collector/backend/OpinionCollectorBackendApplication.java
@@ -1,16 +1,18 @@
 package pl.opinion_collector.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class OpinionCollectorBackendApplication {
     /**
      * Application start method
+     *
      * @param args init app arguments
      */
     public static void main(String[] args) {
         SpringApplication.run(OpinionCollectorBackendApplication.class, args);
     }
-
 }
