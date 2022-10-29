006695c5e5375d07371413a7743f3b4331f0fb12
Tommy <afteryou725@gmail.com>
2022-10-29 19:39:52 +0200

    Initial commit

---
+++ src/main/java/pl/opinion_collector/backend/OpinionCollectorBackendApplication.java
@@ -0,0 +1,16 @@
+package pl.opinion_collector.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinionCollectorBackendApplication {
+    /**
+     * Application start method
+     * @param args init app arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(OpinionCollectorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/opinion_collector/backend/OpinionCollectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.opinion_collector.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinionCollectorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
