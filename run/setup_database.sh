fe0ba86ca4a691c4b02a7a2ce39795b7389497aa
Tommy <afteryou725@gmail.com>
2022-12-04 12:05:17 +0100

    Remove JAR build from run script.

--- run/setup_database.sh
+++ run/setup_database.sh
@@ -30,39 +30,9 @@ build_docker() {
     -p 5432:5432 "$docker_image_name"
 
   echo "-- DATABASE CONFIGURATION FINISHED --"
-
-  run_app "$db_user_name" "$db_password"
-}
-
-run_app() {
-  echo "Build JAR application first using mvn clean install"
-  echo "Note: Remember to include spring.datasource.username and password fields in application.yml"
-  read -rp "JAR file path: " jar_file_path
-  if ! [ -f "$jar_file_path" ]; then
-    echo "File $jar_file_path not exists. Exiting..."
-    exit
-  fi
-
-  java -jar "$jar_file_path" --spring.datasource.username="$1" --spring.datasource.password="$2"
+  echo "You can now build and run your app using given credentials."
 }
 
 echo "-- DATABASE CONFIGURATION --"
 echo "Note: Make sure you have Docker installed."
-
-read -rp "Do you have database docker image? (y/n): " docker_installed
-if [ "$docker_installed" == 'n' ]; then
-  build_docker
-elif [ "$docker_installed" == 'y' ]; then
-  read -rp "Database username: " db_user_name
-  read -rp "Database password: " db_password
-
-  if ! [[ "$db_user_name" && "$db_password" ]]; then
-    echo "Missing arguments. Exiting..."
-    exit
-  fi
-  run_app "$db_user_name" "$db_password"
-else
-  echo "Wrong character: $docker_installed. Exiting..."
-  exit
-fi
-
+build_docker
