f519419c322af23f8a65a5ca73b1d0358f8366a8
Tommy <afteryou725@gmail.com>
2022-12-17 14:56:16 +0100

    Fix id duplication.

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -1,26 +1,26 @@
-insert into category values (1,'Smartfony',true);
-insert into category values (2,'Laptopy',true);
-insert into category values (3,'Pozdespoly komputerowe',false);
+insert into category values (default, 'Smartfony',true);
+insert into category values (default, 'Laptopy',true);
+insert into category values (default, 'Pozdespoly komputerowe',false);
 
-insert into "user" values (1,true,'123@123.pl','Uzytkownik','Nr1','haslo123','www.zdjecie.pl');
-insert into "user" values (2,false,'321@321.com','Uzytkownik2','Nr2','12321','www.test.pl');
-insert into "user" values (3,false,'test@test.gg','User','Testowy','qwerty123','www.www.pl');
+insert into "user" values (default, true,'admin@admin.com','Uzytkownik','Nr1','$2y$10$TpEZyGw9kU4bBDhTgDjU4ebSFi0j3yoBz/5IOoFLLxd.h9lY2x3ge','www.zdjecie.pl'); -- Haslo: admin
+insert into "user" values (default, false,'321@321.com','Uzytkownik2','Nr2','12321','www.test.pl');
+insert into "user" values (default, false,'test@test.gg','User','Testowy','qwerty123','www.www.pl');
 
-insert into product  values(1,'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',null,'www.picture.url','skusku',true,1);
-insert into product values (2,'Procesor Intel, dysk tysionc, cztery ka xD','MacBook Air M1',null,'macbook.url','skuu',true,2);
-insert into product values(3,'Swietnie kopie bitkojna','GeForceRTX4090',null,'picture','sku321',false,3);
+insert into product  values(default, 'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',null,'www.picture.url','skusku',true,1);
+insert into product values (default, 'Procesor Intel, dysk tysionc, cztery ka xD','MacBook Air M1',null,'macbook.url','skuu',true,2);
+insert into product values(default, 'Swietnie kopie bitkojna','GeForceRTX4090',null,'picture','sku321',false,3);
 
-insert into opinion values (1,'["Super jest"]',5,null,1,1);
-insert into opinion values (2,'["No tak srednio bym powiedzial"]',1,'["zdjecieSlabegoProduktu.pl"]',2,1);
-insert into opinion values (3,'["Polecam serdecznie. Pieknie dogrzewa w domku"]',5,null,3,2);
+insert into opinion values (default, '["Super jest"]',5,null,1,1);
+insert into opinion values (default, '["No tak srednio bym powiedzial"]',1,'["zdjecieSlabegoProduktu.pl"]',2,1);
+insert into opinion values (default, '["Polecam serdecznie. Pieknie dogrzewa w domku"]',5,null,3,2);
 
-insert into suggestion values (1,'No ja to bym dorzucil piaty aparat',1,null,1,2);
-insert into suggestion values (2,'Moze troche drozej? Bo musze tylko 3 miesiace pracowac zeby kupic',2,null,1,2);
-insert into suggestion values (3,'Przecietny mu**yn wykopalby tego bitkojna szybciej. Wiencej mocy!',3,null,1,3);
+insert into suggestion values (default, 'No ja to bym dorzucil piaty aparat',1,null,1,2);
+insert into suggestion values (default, 'Moze troche drozej? Bo musze tylko 3 miesiace pracowac zeby kupic',2,null,1,2);
+insert into suggestion values (default, 'Przecietny mu**yn wykopalby tego bitkojna szybciej. Wiencej mocy!',3,null,1,3);
 
-insert into review values (1,'Bedzie i 6 aparat','done',1);
-insert into review values (2,'Zwiekszamy cene, portfel wytrzyma','done',2);
-insert into review values (3,'W takim razie prosze wyposazyc sie w mu**yna','declined',3);
+insert into review values (default, 'Bedzie i 6 aparat','done',1);
+insert into review values (default, 'Zwiekszamy cene, portfel wytrzyma','done',2);
+insert into review values (default, 'W takim razie prosze wyposazyc sie w mu**yna','declined',3);
 
 update suggestion set review_review_id=1 where suggestion_id=1;
 update suggestion set review_review_id=2 where suggestion_id=2;
