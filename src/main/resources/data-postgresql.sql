293f269d0e7aa6d7cd8a75ae18dd6ed8ea77fc80
Tommy <afteryou725@gmail.com>
2023-01-11 21:01:59 +0100

    Add users photos.

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -2,9 +2,9 @@ insert into category values (default, 'Smartphones',true);
 insert into category values (default, 'Laptops',true);
 insert into category values (default, 'Hardware',false);
 
-insert into "user" values (default, true,'admin@admin.com','Uzytkownik','Nr1','$2a$10$Azj6vAZcW7lAKcC5zhQNqurMi3wZHivjLXoWrFq6XyqQqgMeSKEPy','www.zdjecie.pl'); -- Password: admin123 | jwtSecret: mySecretKey
-insert into "user" values (default, false,'321@321.com','Uzytkownik2','Nr2','$2a$10$4sMsUd3mETOBM2Zd109Z6.X6v0E0Q7Mul5si3Us0ZQLGtsCXz1qIa','www.test.pl'); -- Password: 12345678 | jwtSecret: mySecretKey
-insert into "user" values (default, false,'test@test.gg','User','Testowy','$2a$10$ccpn9Ue5AtrJoIeFVLDLo.HiuPqNbJH08BM0lbxCvr4vE5Qtta7bC','www.www.pl'); -- Password: qwerty123 | jwtSecret: mySecretKey
+insert into "user" values (default, true,'admin@admin.com','Uzytkownik','Nr1','$2a$10$Azj6vAZcW7lAKcC5zhQNqurMi3wZHivjLXoWrFq6XyqQqgMeSKEPy','https://img2.looper.com/img/gallery/the-best-episodes-of-breaking-bad/l-intro-1609890074.jpg'); -- Password: admin123 | jwtSecret: mySecretKey
+insert into "user" values (default, false,'321@321.com','Uzytkownik2','Nr2','$2a$10$4sMsUd3mETOBM2Zd109Z6.X6v0E0Q7Mul5si3Us0ZQLGtsCXz1qIa','https://i0.wp.com/applauss.com/wp-content/uploads/2017/02/jesse-pinkman-quiz.jpg?fit=1200%2C800&ssl=1'); -- Password: 12345678 | jwtSecret: mySecretKey
+insert into "user" values (default, false,'test@test.gg','User','Testowy','$2a$10$ccpn9Ue5AtrJoIeFVLDLo.HiuPqNbJH08BM0lbxCvr4vE5Qtta7bC','https://i.ytimg.com/vi/V94Y1CqiBfA/hqdefault.jpg'); -- Password: qwerty123 | jwtSecret: mySecretKey
 
 insert into product  values(default, 'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',5.0,'https://www.mistershopking.com/8877/samsung-galaxy-s20-g980f-8128gb-dual-sim-grey-italia.jpg','skusku',true,1);
 insert into product values (default, 'Procesor Intel, 1000GB, 4K display','MacBook Air M1',1.0,'https://www.macworld.com/wp-content/uploads/2021/03/macbook-air-2108-hero2-100779122-orig-7.jpeg?quality=50&strip=all','skuu',true,2);
