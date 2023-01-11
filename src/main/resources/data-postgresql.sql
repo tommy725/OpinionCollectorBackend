b8c927da01ee6f2708479d64691c651bdfd2bfa8
Tommy <afteryou725@gmail.com>
2023-01-11 20:54:32 +0100

    Encrypt passwords; Translate opinions and descriptions in data-init script

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -1,26 +1,26 @@
-insert into category values (default, 'Smartfony',true);
-insert into category values (default, 'Laptopy',true);
-insert into category values (default, 'Pozdespoly komputerowe',false);
+insert into category values (default, 'Smartphones',true);
+insert into category values (default, 'Laptops',true);
+insert into category values (default, 'Hardware',false);
 
-insert into "user" values (default, true,'admin@admin.com','Uzytkownik','Nr1','$2y$10$TpEZyGw9kU4bBDhTgDjU4ebSFi0j3yoBz/5IOoFLLxd.h9lY2x3ge','www.zdjecie.pl'); -- Password: admin
-insert into "user" values (default, false,'321@321.com','Uzytkownik2','Nr2','$2y$10$urrJZCn8CmJlmmfrhR64guBFZa8EB7SpI5ZubLXKA5sXwW3ZvM/qe','www.test.pl'); -- Password: 12321
-insert into "user" values (default, false,'test@test.gg','User','Testowy','$2y$10$dEaQqErGfxX2R3LeeT1G8eeKQDEuhZvWqii862a9JPciF7JhVWioG','www.www.pl'); -- Password: qwerty123
+insert into "user" values (default, true,'admin@admin.com','Uzytkownik','Nr1','$2a$10$Azj6vAZcW7lAKcC5zhQNqurMi3wZHivjLXoWrFq6XyqQqgMeSKEPy','www.zdjecie.pl'); -- Password: admin123 | jwtSecret: mySecretKey
+insert into "user" values (default, false,'321@321.com','Uzytkownik2','Nr2','$2a$10$4sMsUd3mETOBM2Zd109Z6.X6v0E0Q7Mul5si3Us0ZQLGtsCXz1qIa','www.test.pl'); -- Password: 12345678 | jwtSecret: mySecretKey
+insert into "user" values (default, false,'test@test.gg','User','Testowy','$2a$10$ccpn9Ue5AtrJoIeFVLDLo.HiuPqNbJH08BM0lbxCvr4vE5Qtta7bC','www.www.pl'); -- Password: qwerty123 | jwtSecret: mySecretKey
 
-insert into product  values(default, 'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',null,'www.picture.url','skusku',true,1);
-insert into product values (default, 'Procesor Intel, dysk tysionc, cztery ka xD','MacBook Air M1',null,'macbook.url','skuu',true,2);
-insert into product values(default, 'Swietnie kopie bitkojna','GeForceRTX4090',null,'picture','sku321',false,3);
+insert into product  values(default, 'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',5.0,'https://www.mistershopking.com/8877/samsung-galaxy-s20-g980f-8128gb-dual-sim-grey-italia.jpg','skusku',true,1);
+insert into product values (default, 'Procesor Intel, 1000GB, 4K display','MacBook Air M1',1.0,'https://www.macworld.com/wp-content/uploads/2021/03/macbook-air-2108-hero2-100779122-orig-7.jpeg?quality=50&strip=all','skuu',true,2);
+insert into product values(default, 'Mines bitcoin perfectly','GeForceRTX4090',5.0,'https://techstory.in/wp-content/uploads/2021/12/Newsycanuse-1024x626.jpg','sku321',false,3);
 
-insert into opinion values (default, '["Super jest"]',5,null,1,1);
-insert into opinion values (default, '["No tak srednio bym powiedzial"]',1,'["zdjecieSlabegoProduktu.pl"]',2,1);
-insert into opinion values (default, '["Polecam serdecznie. Pieknie dogrzewa w domku"]',5,null,3,2);
+insert into opinion values (default, '["Its great"]',5,null,1,1);
+insert into opinion values (default, '["Not so good I would say"]',1,'https://i.ytimg.com/vi/nGHpgArkKOw/maxresdefault.jpg',2,1);
+insert into opinion values (default, '["Really recommend. Heats all my house."]',5,null,3,2);
 
-insert into suggestion values (default, 'No ja to bym dorzucil piaty aparat',1,null,1,2);
-insert into suggestion values (default, 'Moze troche drozej? Bo musze tylko 3 miesiace pracowac zeby kupic',2,null,1,2);
-insert into suggestion values (default, 'Przecietny mu**yn wykopalby tego bitkojna szybciej. Wiencej mocy!',3,null,1,3);
+insert into suggestion values (default, 'I would add 5th camera',1,null,1,2);
+insert into suggestion values (default, 'Maybe more expensive? I have to work only 3 months to buy it...',2,null,1,2);
+insert into suggestion values (default, 'Average **N word here** would mine that bitcoin faster. More power!',3,null,1,3);
 
-insert into review values (default, 'Bedzie i 6 aparat','done',1);
-insert into review values (default, 'Zwiekszamy cene, portfel wytrzyma','done',2);
-insert into review values (default, 'W takim razie prosze wyposazyc sie w mu**yna','declined',3);
+insert into review values (default, '6th camera on its way','done',1);
+insert into review values (default, 'Increasing the price, wallet will resist','done',2);
+insert into review values (default, 'So please equip yourself with **N word here**','declined',3);
 
 update suggestion set review_review_id=1 where suggestion_id=1;
 update suggestion set review_review_id=2 where suggestion_id=2;
