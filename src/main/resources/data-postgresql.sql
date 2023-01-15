4089b6669f39a614d3c7fcdbcc5adf7e3d0d4f6f
Tommy <afteryou725@gmail.com>
2023-01-15 11:54:15 +0100

    Sample data avg fix

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -6,13 +6,14 @@ insert into "user" values (default, true,'admin@admin.com','Uzytkownik','Nr1','$
 insert into "user" values (default, false,'321@321.com','Uzytkownik2','Nr2','$2a$10$4sMsUd3mETOBM2Zd109Z6.X6v0E0Q7Mul5si3Us0ZQLGtsCXz1qIa','https://i0.wp.com/applauss.com/wp-content/uploads/2017/02/jesse-pinkman-quiz.jpg?fit=1200%2C800&ssl=1'); -- Password: 12345678 | jwtSecret: mySecretKey
 insert into "user" values (default, false,'test@test.gg','User','Testowy','$2a$10$ccpn9Ue5AtrJoIeFVLDLo.HiuPqNbJH08BM0lbxCvr4vE5Qtta7bC','https://i.ytimg.com/vi/V94Y1CqiBfA/hqdefault.jpg'); -- Password: qwerty123 | jwtSecret: mySecretKey
 
-insert into product  values(default, 'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',5.0,'https://www.mistershopking.com/8877/samsung-galaxy-s20-g980f-8128gb-dual-sim-grey-italia.jpg','skusku',true,1);
-insert into product values (default, 'Procesor Intel, 1000GB, 4K display','MacBook Air M1',1.0,'https://www.macworld.com/wp-content/uploads/2021/03/macbook-air-2108-hero2-100779122-orig-7.jpeg?quality=50&strip=all','skuu',true,2);
-insert into product values(default, 'Mines bitcoin perfectly','GeForceRTX4090',5.0,'https://techstory.in/wp-content/uploads/2021/12/Newsycanuse-1024x626.jpg','sku321',false,3);
+insert into product  values(default, 'Ekran "6,5cala 6/128GB"','Samsung Galaxy S20',3.0,'https://www.mistershopking.com/8877/samsung-galaxy-s20-g980f-8128gb-dual-sim-grey-italia.jpg','skusku',true,1);
+insert into product values (default, 'Procesor Intel, 1000GB, 4K display','MacBook Air M1',4.0,'https://www.macworld.com/wp-content/uploads/2021/03/macbook-air-2108-hero2-100779122-orig-7.jpeg?quality=50&strip=all','skuu',true,2);
+insert into product values(default, 'Mines bitcoin perfectly','GeForceRTX4090',1.0,'https://techstory.in/wp-content/uploads/2021/12/Newsycanuse-1024x626.jpg','sku321',false,3);
 
 insert into opinion values (default, '["Its great"]','Noice :O',null,5,'https://i.ytimg.com/vi/nGHpgArkKOw/maxresdefault.jpg',1,1);
 insert into opinion values (default, null,'Not so good I would say','["Useless", "Bad quality"]',1,null,1,3);
 insert into opinion values (default, '["Really recommend. Heats all my house."]','Wow!','["Expensive"]',4,null,2,1);
+insert into opinion values (default, null,'Really bad!',null,1,null,3,1);
 
 insert into suggestion values (default, 'I would add 5th camera',1,null,1,2);
 insert into suggestion values (default, 'Maybe more expensive? I have to work only 3 months to buy it...',2,null,1,2);
